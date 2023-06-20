426422e580b2a382b10cbe4b7512b59bfa3d0a4a
Felix Chen <Felixx.Chenn@gmail.com>
2023-06-20 13:21:45 -0400

    First function solved

---
+++ mathFunctions.test.js
@@ -0,0 +1,25 @@
+const mathFunctions = require("./mathFunctions");
+
+describe("add function returns the sum of two numbers", () => {
+  test("add(1, 1) => 2", () => {
+    expect(mathFunctions.add(1, 1)).toBe(2);
+  });
+});
+
+describe("subtract function returns the difference of two numbers", () => {
+  test("subtract(1, 1) => 0", () => {
+    expect(mathFunctions.subtract(1, 1)).toBe(0);
+  });
+});
+
+describe("multiply function returns the product of two numbers", () => {
+  test("multiply(1,5) => 5", () => {
+    expect(mathFunctions.multiply(1, 5)).toBe(5);
+  });
+});
+
+describe("divide function returns the quotient of first number by the second number", () => {
+  test("divide(1,5) => 0.20", () => {
+    expect(mathFunctions.divide(1, 5)).toBe(0.2);
+  });
+});
